fdd32ae54b1d0e37f26b675c298bc409e0a6f8b0
soroush <soroush.r80@gmail.com>
2024-03-06 09:56:19 +0330

    hw week - (1)

---
+++ main.js
@@ -0,0 +1,89 @@
+// Q1
+// //A
+let length = 16 ;
+console.log("typeof (A): " + typeof length);
+// B
+let name = ("Jack")
+console.log("typeof (B): " + typeof name);
+// C
+let number = '120' ;
+console.log("typeof (C): " + typeof number);
+// // D
+var y = 'undefined' ;
+console.log("typeof (D): " + typeof y );
+// // E
+let z = undefined ;
+console.log("typeof (E): " + typeof z );
+// // F
+let isAdmin = true ;
+console.log("typeof (F): " + typeof isAdmin );
+// // G
+const array = [1, 2, 'john' , true, '201', undefined] ;
+console.log("typeof (G): " + typeof array );
+// // H
+const x = {
+firstName : 'ali' ,
+lastName : 'hosseini'
+} ;
+console.log("typeof (H): " + typeof x );
+
+
+// // Q2 For number
+// let input = +prompt("pls Enter your input : ");
+// alert(" type Of input : " + typeof(input));
+ 
+// // Q2 for string
+// let input = prompt("pls Enter your input : ");
+// alert(" type Of input : " + typeof(input));
+
+// let userName = prompt(" pls enter the UserName :  ");
+
+
+// Q3
+// let userName = prompt("Enter your name:");
+
+// let userName_check = confirm("Please check your name '" + userName + "' ?  ");
+
+// if (userName_check) {
+    
+//     alert( " Hello '" + userName + "'");
+// } 
+// else {
+//     alert(" No ! Please try again.");
+// }
+
+// q4
+// let numberOne = +prompt(" Please Enter The Number one : ");
+// let numberTwo = +prompt(" Please Enter The Number two : ");
+
+// alert(" Result :" + ( numberOne + numberTwo) );
+
+//Q10
+
+
+// const person = {
+    
+//     userfirstName : prompt( "What's your First Name ? "),
+//     userlastName : prompt( " What's your Last Name ? " )
+// };
+
+// alert(`First Name: ${person.userfirstName}\nLast Name: ${person.userlastName}`);
+
+// alert( "first name : " + person.userfirstName + "\nlast name : " + person.userlastName )
+
+//Q11 
+
+// alert(' hello\tmaktab "');
+// console.log("hello\tmaktab");
+// const val1 = "a" && 0 && ""
+
+// // console.log(val1);
+// let randomNumber2 = +prompt("enter the")
+// const randomNumber = (min , max)=>{
+//     return Math.floor(Math.random() * (max - min + 1)) + min
+//     }
+//     console.log(randomNumber);
+
+// let num1 = 20 ; 
+
+// console.log(`result is : ${num1}`);
